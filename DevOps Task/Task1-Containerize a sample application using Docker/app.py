330285834476061b45121b7e0e693d676645c4ed
Shikha Sharma <shikhasharmaj2005@gmail.com>
2026-08-31 20:57:16 +0530

    Add Docker DevOps tasks

---
+++ DevOps Task/Task1-Containerize a sample application using Docker/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from my Dockerized application!"
+
+@app.route("/about")
+def about():
+    return "This application is running inside a Docker container."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
